a32f2619b1c23821d40dc935463926bc7e7fc847
Apu Fouzder <apufouzdar@gmail.com>
2021-02-07 20:15:10 +0600

    complete

--- app.js
+++ app.js
@@ -1,187 +1,67 @@
+/*
+Using all arrow function.
+Using 1 forEach loop. 
+Using 1 addEventListener.
+Using JavaScript template strings.
+*/
+
+// main / meals result function .
+const mealsResult = () => {
+    document.getElementById("search-result").innerHTML = '';
+    const searchName = document.getElementById("search-title").value;
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchName}`)
+        .then(res => res.json())
+        .then(data => displaySearchResult(data.meals))
+        .catch(error => {
+            document.getElementById("search-result").innerHTML = `
+            <div class="custom-design">
+                <span class="error"><i class="fas fa-exclamation-triangle"></i></span>
+                <h3>Sorry, "${searchName}" meals could not be found. Please try again..</h3>
+            </div>
+        `;
+        document.getElementById("singleMealDetails").style.display = "none";
+        })
+
+    // Display search result function
+    const displaySearchResult = meals => {
+        // added forEach loop
+        meals.forEach(mealNam => {
+            //template strings added
+            document.getElementById("search-result").innerHTML += `
+                <div class="col-md-3 custom-css text-center" onclick="renderMealsInfo('${mealNam.strMealThumb}', '${mealNam.strCategory}', '${mealNam.strIngredient1}', '${mealNam.strIngredient2}', '${mealNam.strIngredient3}', '${mealNam.strIngredient4}', '${mealNam.strIngredient5}', '${mealNam.strIngredient6}', '${mealNam.strIngredient7}', '${mealNam.strIngredient8}', '${mealNam.strIngredient9}', '${mealNam.strIngredient10}')">
+                    <div class="card h-100">
+                        <img class="card-img-top" src="${mealNam.strMealThumb}" alt="">
+                        <div class="card-body">
+                            <h5 id="card-title" class="card-title text-center">${mealNam.strCategory}</h5>
+                        </div>
+                    </div>
+                </div>
+             `;
+        });
+    }   
+}
+
+
+// Search button addEventListener and call main function.
 document.getElementById("search-btn").addEventListener("click", mealsResult);
 
-function mealsResult(){
-    document.getElementById("search-result").innerHTML = '';
-
-const searchName = document.getElementById("search-title").value;
-
-fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchName}`)
-.then(res => res.json())
-.then(data => displaySearchResult(data.meals))
-
-const displaySearchResult = meals =>{
-    console.log(meals[0]);
-    ///const searchResult = document.getElementById("search-result");
-    meals.forEach(mealNam => {
-        //const mealsName = meals.meals[0];
-        //console.log(mealsName);
-        //const mealTitle = mealsName.strCategory;
-        //const mealImage = mealsName.strMealThumb;
-        // console.log(mealTitle);
-        // console.log(mealNam);
-        // const infoDiv = document.createElement('div');
-
-        document.getElementById("search-result").innerHTML += `
-        <div class="col-md-3">
-            <div class="card h-100">
-                <img class="card-img-top" src="${mealNam.strMealThumb}" alt="">
-                <div class="card-body">
-                    <h5 id="card-title" class="card-title text-center">${mealNam.strCategory}</h5>
-                    <button onclick="renderMealsInfo('${mealNam.strMealThumb}', '${mealNam.strCategory}', '${mealNam.strArea}')">click</button>
-                </div>
-            </div>
-        </div>
-        `;
-    });
-}
-}
-
-// const displaySingleMeal = imgName =>{
-//     const url = `${imgName}`;
-//     fetch(url)
-//     .then(res => res.json())
-//     .then(data => renderMealsInfo(data.meals.meals))
-// }
-
-const renderMealsInfo = (mealNam, mealNam2, mealNam3)=>{
-    console.log('meal', mealNam);
+// single meals info function
+const renderMealsInfo = (mealNam, mealNam2, mealNam3, mealNam4, mealNam5, mealNam6, mealNam7, mealNam8, mealNam9, mealNam10, mealNam11, mealNam12) => {
     const singleMealDetails = document.getElementById("singleMealDetails");
-    singleMealDetails.innerHTML =`
-    <img class="card-img-top" src="${mealNam}" alt="">
-    <h1 id="" class="card-title">Name: ${mealNam2}</h1>
-    <h2 id="" class="card-text">Area Name: ${mealNam3}</h2>
+    //singleMealDetails.style.display = "block";
+    singleMealDetails.innerHTML = `
+        <img class="card-img-top pb-3" src="${mealNam}" alt="">
+        <h3 id="" class="card-title pl-4">${mealNam2}</h3>
+        <h5 id="" class="card-text pl-4 pb-2">${mealNam3}</h5>
+        <p class="pl-4"> <span class="icon"><i class="fas fa-check"></i></span> ${mealNam4}</p>
+        <p class="pl-4"> <span class="icon"><i class="fas fa-check"></i></span> ${mealNam5}</p>
+        <p class="pl-4"> <span class="icon"><i class="fas fa-check"></i></span> ${mealNam6}</p>
+        <p class="pl-4"> <span class="icon"><i class="fas fa-check"></i></span> ${mealNam7}</p>
+        <p class="pl-4"> <span class="icon"><i class="fas fa-check"></i></span> ${mealNam8}</p>
+        <p class="pl-4"> <span class="icon"><i class="fas fa-check"></i></span> ${mealNam9}</p>
+        <p class="pl-4"> <span class="icon"><i class="fas fa-check"></i></span> ${mealNam10}</p>
+        <p class="pl-4"> <span class="icon"><i class="fas fa-check"></i></span> ${mealNam11}</p>
+        <p class="pl-4 pb-4"> <span class="icon"><i class="fas fa-check"></i></span> ${mealNam12}</p>
     `;
 }
 
-
-
-/*
-document.getElementById("search-btn").addEventListener("click", mealsResult);
-
-function mealsResult(){
-    document.getElementById("search-result").innerHTML = '';
-
-const searchName = document.getElementById("search-title").value;
-
-fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchName}`)
-.then(res => res.json())
-.then(data => displaySearchResult(data))
-
-const displaySearchResult = meal =>{
-    console.log(meal);
-    for (let i = 0; i < meal.meals.length; i++) {
-        const mealsName = meal.meals[0];
-        console.log(mealsName);
-        const mealTitle = mealsName.strCategory;
-        const mealImage = mealsName.strMealThumb;
-        // console.log(mealTitle);
-        // console.log(mealImage);
-
-        document.getElementById("search-result").innerHTML += `
-        <div class="col-md-3">
-            <div class="card h-100">
-                <img class="card-img-top" src="${mealImage}" alt="">
-                <div class="card-body">
-                    <h5 id="card-title" class="card-title text-center">${mealTitle}</h5>
-                </div>
-            </div>
-        </div>
-        `;
-    }
-}
-}
-*/
-
-
-
-
-
-
-
-/*
-fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
-.then(res => res.json())
-.then(data => {
-    console.log(data);
-    for (let i = 0; i < data.categories.length; i++) {
-        const categoriesTitle = data.categories[i].strCategory;
-        const categoriesImage = data.categories[i].strCategoryThumb;
-        console.log(categoriesTitle);
-        console.log(categoriesImage);
-
-        document.getElementById("search-result").innerHTML += `
-        <div class="col-md-3">
-            <div class="card h-100">
-                <img class="card-img-top" src="${categoriesImage}" alt="">
-                <div class="card-body">
-                    <h5 id="card-title" class="card-title text-center">${categoriesTitle}</h5>
-                </div>
-            </div>
-        </div>
-        `;
-
-        if(i == 7){
-            break;
-        }
-    }
-})
-*/
-
-
-
-
-
-
-
-/*
-fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
-.then(res => res.json())
-.then(data => displayFood(data))
-    
-const displayFood = data =>{
-    const categories = data.categories;
-    const categories0 = data.categories[0];
-    const categoriesImg = data.categories[0].strCategoryThumb;
-    const categoriesTitle = data.categories[0].strCategory;
-    
-
-    document.getElementById("Img").innerHTML = `
-    <img id="" src="${categoriesImg}" class="card-img-top" alt="...">
-    `;
-    document.getElementById("card-title").innerText = categoriesTitle;
-    
-    console.log(categoriesImg);
-    console.log(categories0);
-    //console.log(categories);
-}
-*/
-
-
-
-
-
-
-
-
-// fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood')
-// .then(res => res.json())
-// .then(data =>{
-//     console.log('List 1', data);
-// })
-// fetch('https://www.themealdb.com/images/media/meals/llcbn01574260722.jpg/preview')
-// .then(res => res.json())
-// .then(data =>{
-//     console.log('List img', data);
-// })
-
-
-// fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata')
-// .then(res => res.json())
-// .then(data =>{
-//     const name = data.meals;
-//     console.log('List 3', name[0]);
-
-
-
-//     console.log('List 3', name[0].strMealThumb);
-// })
